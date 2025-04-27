41847ac608948264181d01e6c529d7d7bdcbd9e9
handikatriarlan <handikaarlan@gmail.com>
2025-04-27 16:21:13 +0700

    chore(cors): remove cors config

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { corsConfig } from './config/cors.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors(corsConfig.getOptions());
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
