d031e879f66ab74b7e4c8cf787ff6f98ea17ae80
handikatriarlan <handikaarlan@gmail.com>
2025-04-27 16:17:17 +0700

    chore: remove console log

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT || 3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  // console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
